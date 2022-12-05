f7a2e6ded34d36ab5c576d6ffe1d15e445e884e6
VVSRevolution <viniciovieira.vps@hotmail.com>
2022-12-05 15:30:37 -0300

    fix token lexema

--- scanner.py
+++ scanner.py
@@ -1,81 +1,103 @@
-def scanner():
-    file = open('code.txt', 'r')
+def scanner(file):
     char = file.read(1)
     #print(char)
+
+    while(char == '\t' or char == '\n' or char == ' '): 
+        char = file.read(1)
+
+    if(char.isnumeric()):
+        pass
+        #return token lexema tipo
+
+    if(char == '\"'):
+        lexema = char
+        char = file.read(1)
+        while(char != '\"'):
+            char = file.read(1)
+            lexema += char
+            if not char:
+                return "ERRO \" não foi fechada"
+        return f"Lit {lexema}"
+        
+
+    if(char.isalpha()):
+        lexema = char
+        char = file.read(1)
+        while not (char == '\t' or char == '\n' or char == ' ' or not char) and (char.isalpha() or char.isnumeric() or char =='_'):
+            lexema += char
+            char = file.read(1)
+        if(char == '\t' or char == '\n' or char == ' ' or not char):
+            return f"id {lexema}"
+        else:
+            lexema += char
+            return f"ERRO id \"{lexema}\" não valido, id não pode ter \"{char}\""
+        #return token lexema tipo
+
+    if(char == '{'):
+        lexema = char
+        while(char != '}'):
+            char = file.read(1)
+            lexema += char
+            if not char:
+                return " { não foi fechada"
+        return f"Comentario {lexema}"
     
     if not char:
         return "EOF"
 
-    if(char == '/t' or char == '/n' or char == ' '): 
-        return "tab or line or space"
-
-    if(char.isalpha()):
-        pass
-
-    if(char.isnumeric()):
-        pass
-
-    if(char == '\"'):
-        while(char != '\"'):
-            char = file.read(1)
-            if not char:
-                return "\" não foi fechada"
-        return "Constante literal"
-
-    if(char == '{'):
-        while(char != '}'):
-            char = file.read(1)
-            if not char:
-                return " { não foi fechada"
-        return "Comentario"
-
-    if(char == '+'):
-        return "mais"
-
-    if(char == '-'):
-        return "menos"
-
-    if(char == '*'):
-        return "vezes"
-
-    if(char == '/'):
-        return "divisao"
-
-    if(char == '='):
-        return "igual"
-
-    if(char == '('):
-        return "abre parenteses"
-
-    if(char == ')'):
-        return "fecha parenteses"
-
-    if(char == ';'):
-        return "ponto e virgula"
-
-    if(char == ','):
-        return "virgula"
-
-    if(char == '>'):
-        char = file.read(1)
-        if(char == '='):
-            return "maior igual"
-        if(char == '/t' or char == '/n' or char == ' ' or not char): #not char necessário ?
-            return "maior"
-        else:
-            return f"ERROR \">{char}\" não conhecido"
     if(char == '<'):
         char = file.read(1)
         if(char == '>'):
-            return "<>"
-        if(char == '-'):
-            return "<-"
+            return "OPR <>"
         if(char == '='):
-            return "menor igual"  
-        if(char == '/t' or char == '/n' or char == ' ' or not char): #not char necessário ?
-            return "menor"
+            return f"OPR <="
+        if(char == '-'):
+            return f"ATR <-"
+        if(char == '\t' or char == '\n' or char == ' ' or not char): #not char necessário ?
+            return f"OPR <"
         else:
-            return f"ERROR \">{char}\" não conhecido"
+            return f"ERRO \">{char}\" não conhecido"
+    if(char == '='):
+        return f"OPR {char}"
+    if(char == '>'):
+        char = file.read(1)
+        if(char == '='):
+            return "OPR >="
+        if(char == '\t' or char == '\n' or char == ' ' or not char): #not char necessário ?
+            return "OPR >"
+        else:
+            return f"ERRO \">{char}\" não conhecido"
 
 
-print(scanner())
+    if(char == '+'):
+        return f"OPA {char}"
+    if(char == '-'):
+        return f"OPA {char}"
+    if(char == '*'):
+        return f"OPA {char}"
+    if(char == '/'):
+        return f"OPA {char}"
+
+
+    if(char == '('):
+        return "AB_P ("
+
+    if(char == ')'):
+        return "FC_P )"
+
+    if(char == ';'):
+        return "PT_V ;"
+
+    if(char == ','):
+        return "VIR ,"
+    return f"ERRO {char} não conhecido"
+
+
+file = open('code.txt', 'r')
+Scanner = scanner(file)
+while(Scanner != "EOF"):
+    print(Scanner)
+    Scanner = scanner(file)
+
+# ADD linha e culuna do erro
+
